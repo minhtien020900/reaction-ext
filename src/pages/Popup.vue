a16285e44e4c468fdd7272a97e3956a1847327c6
[Lampart] Vo Minh Tien <minh_tien@lampart-vn.com>
2024-01-18 18:16:15 +0700

    update

--- src/pages/Popup.vue
+++ src/pages/Popup.vue
@@ -1,10 +1,45 @@
 <script setup>
+import {ref} from "vue";
+
+const tab = ref('option-1')
+const items = ref( [
+  { text: 'Real-Time', icon: 'mdi-clock' },
+  { text: 'Audience', icon: 'mdi-account' },
+  { text: 'Conversions', icon: 'mdi-flag' },
+])
 </script>
 
 <template>
-  <p>hehe</p>
+
+  <v-card
+      class="hihi px-3"
+      max-width="300"
+  >
+    <v-list density="compact">
+      <v-list-subheader>REPORTS</v-list-subheader>
+
+      <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :value="item"
+          color="primary"
+      >
+        <template v-slot:prepend>
+          <v-icon :icon="item.icon"></v-icon>
+          <p class="v-divider">ssss</p>
+        </template>
+
+        <v-list-item-title v-text="item.text"></v-list-item-title>
+
+      </v-list-item>
+
+    </v-list>
+  </v-card>
+
 </template>
 
-<style>
-
+<style scoped >
+.hihi{
+  width: 400px;
+}
 </style>
--- src/popup.js
+++ src/popup.js
@@ -1,4 +1,22 @@
 import Popup from "./pages/Popup.vue";
 import { createApp } from "vue";
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from "vuetify/lib/iconsets/mdi";
+import "@mdi/font/css/materialdesignicons.css"
 
-createApp(Popup).mount("body");
+const vuetify = createVuetify({
+    icons: {
+        defaultSet: "mdi",
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+    components,
+    directives,
+})
+createApp(Popup).use(vuetify).mount("body");
